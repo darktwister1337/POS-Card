d35457a75bc60076fe8f377157ff59f789cc43e3
horkworse <horkworse@gmail.com>
2020-10-12 12:16:57 +0500

    create connection object

--- src/db/connectionMySQL.js
+++ src/db/connectionMySQL.js
@@ -1,58 +1,53 @@
-let Partner = require('../classes/Partner')
-let News = require('../classes/News')
-let User = require('../classes/User')
-const mysql = require('mysql2');
+function DataGetter() {
+    const Partner = require('./classes/Partner');
+    const News = require('./classes/News');
+    const User = require('./classes/User');
+    const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: 'urfu1.paulislava.space',
-    user: 'admin_pos',
-    database: 'admin_pos',
-    password: 'nt8ewv6hxK'
-}).promise();
+    const connection = mysql.createConnection({
+        host: 'urfu1.paulislava.space',
+        user: 'admin_pos',
+        database: 'admin_pos',
+        password: 'nt8ewv6hxK'
+    }).promise();
 
+    this.getPartners = async function() {
 
-const partners = [];
-const news = [];
-const users = [];
+        let result = [];
+        await connection
+        .query('SELECT `id`, `title` AS `name`, `discount` AS `sale`, `discount_full` AS `description`, `addresses`, `coverUri` AS `logo` FROM `card_partners`')
+        .then((x) => {
+            x[0].forEach((el) => result.push(new Partner(el)));
+        });
+        console.log(result);
+        return result;
+    }
 
+    this.getNews = async function() {
 
-async function getPartners(connect) {
+        let result = [];
+        await connection
+        .query('SELECT `id`, `title` AS `name`, `discount` AS `sale`, `discount_full` AS `description`, `addresses`, `coverUri` AS `logo` FROM `card_partners`')
+        .then((x) => {
+            x[0].forEach((el) => result.push(new Partner(el)));
+        });
+        console.log(result);
+        return result;
+    }
 
-    let result = [];
-    await connect
-    .query('SELECT `id`, `title` AS `name`, `discount` AS `sale`, `discount_full` AS `description`, `addresses`, `coverUri` AS `logo` FROM `card_partners`')
-    .then((x) => {
-        x[0].forEach((el) => result.push(new Partner(el)));
-    });
-    console.log(result);
-    return result;
+    this.getUsers = async function() {
+
+        let result = [];
+        await connection
+        .query('SELECT `id`, `title` AS `name`, `discount` AS `sale`, `discount_full` AS `description`, `addresses`, `coverUri` AS `logo` FROM `card_partners`')
+        .then((x) => {
+            x[0].forEach((el) => result.push(new Partner(el)));
+        });
+        console.log(result);
+        return result;
+    }
+
+    this.end = function () {
+        connection.end();
+    };
 }
-
-async function getNews(connect) {
-
-    let result = [];
-    await connect
-    .query('SELECT `id`, `title` AS `name`, `discount` AS `sale`, `discount_full` AS `description`, `addresses`, `coverUri` AS `logo` FROM `card_partners`')
-    .then((x) => {
-        x[0].forEach((el) => result.push(new Partner(el)));
-    });
-    console.log(result);
-    return result;
-}
-
-async function getUsers(connect) {
-
-    let result = [];
-    await connect
-    .query('SELECT `id`, `title` AS `name`, `discount` AS `sale`, `discount_full` AS `description`, `addresses`, `coverUri` AS `logo` FROM `card_partners`')
-    .then((x) => {
-        x[0].forEach((el) => result.push(new Partner(el)));
-    });
-    console.log(result);
-    return result;
-}
-
-
-console.log(getPartners(connection));
-
-connection.end();
